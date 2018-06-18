40e168eca99e6c03fc562db0e8bcdc496760934b
kimihito <tatsurotamashiro@gmail.com>
2018-06-18 10:47:47 +0900

    Fix: tweak Speech attributes

--- lib/kokkai/speech.rb
+++ lib/kokkai/speech.rb
@@ -1,8 +1,14 @@
 require 'kokkai/record'
 module Kokkai
   class Speech < Record
+
     SPEECH_ATTRIBUTES = %i(speech_order speaker speech)
 
     define_attribute_methods(SPEECH_ATTRIBUTES)
+
+    def initialize(raw)
+      super
+      @raw_record = @raw_record.dig("speechRecord")
+    end
   end
 end
