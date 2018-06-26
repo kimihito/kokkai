60d3f0ce9d2c99804abbbe242d3b792e28ea66e3
kimihito <tatsurotamashiro@gmail.com>
2018-06-26 14:54:10 +0900

    Update: change parameter key to snake case

--- spec/kokkai/client_spec.rb
+++ spec/kokkai/client_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe Kokkai::Client  do
   subject(:client) { described_class.new() }
   describe "#speech", vcr: true do
     it "returns speeches" do
-      params = {any: "アベノミクス", speaker: "安倍晋三", startRecord: 1, maximumRecords: 5}
+      params = {any: "アベノミクス", speaker: "安倍晋三", start_record: 1, maximum_records: 5}
       expect(client.speech(params).body).to be_a(Kokkai::Parser)
     end
   end
 
   describe "#meeting", vcr: true do
     it "returns meetings" do
-      params = { nameOfHouse: "両院", nameOfMeeting: "国家基本政策委員会合同審査会" }
+      params = { name_of_house: "両院", name_of_meeting: "国家基本政策委員会合同審査会" }
       expect(client.meeting(params).body).to be_a(Kokkai::Parser)
     end
   end
